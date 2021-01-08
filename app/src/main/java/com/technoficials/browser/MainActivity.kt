b360bf4b2a9506fb177a088e15c86495a58c9af7
TechnoFicials <technoficialsindia@gmail.com>
2021-01-08 17:45:40 +0530

    Refactoring - 08th Jan 2021, 17:44

--- app/src/main/java/com/technoficials/browser/MainActivity.kt
+++ app/src/main/java/com/technoficials/browser/MainActivity.kt
@@ -3,6 +3,10 @@ package com.technoficials.browser
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import android.os.Handler
+import android.view.View
+import android.view.animation.AnimationUtils
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
